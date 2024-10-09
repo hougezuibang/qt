097e5dacf773297898cdeab40ed90c6a61071cec
hougezuibang <3427781715@qq.com>
2024-10-09 09:38:20 +0800

    1

--- UI/计算器.py
+++ UI/计算器.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\PythonProject\UI\counter.ui'
+# Form implementation generated from reading ui file 'd:\PythonProject\UI\计算器.ui'
 #
 # Created by: PyQt5 UI code generator 5.9
 #
