addde9ff8f3ef77e8bcdd91633ca05309aade769
hougezuibang <3427781715@qq.com>
2024-10-31 10:22:03 +0800

    22

---
+++ demo/home_ui.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'd:\PythonProject\demo\home.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_home(object):
+    def setupUi(self, home):
+        home.setObjectName("home")
+        home.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(home)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(60, 20, 691, 431))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.login_btn = QtWidgets.QPushButton(self.frame)
+        self.login_btn.setGeometry(QtCore.QRect(530, 0, 31, 28))
+        self.login_btn.setObjectName("login_btn")
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_2.setGeometry(QtCore.QRect(582, 0, 31, 28))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.exit = QtWidgets.QPushButton(self.frame)
+        self.exit.setGeometry(QtCore.QRect(630, 0, 31, 28))
+        self.exit.setObjectName("exit")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(0, -10, 141, 41))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(85, 170, 255);")
+        self.label.setObjectName("label")
+        self.frame_2 = QtWidgets.QFrame(self.frame)
+        self.frame_2.setGeometry(QtCore.QRect(0, 40, 221, 391))
+        self.frame_2.setStyleSheet("border-radius:20px;\n"
+"background-color: rgb(221, 244, 255);")
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.label_2 = QtWidgets.QLabel(self.frame_2)
+        self.label_2.setGeometry(QtCore.QRect(40, 120, 141, 71))
+        self.label_2.setStyleSheet("\n"
+"color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(255, 0, 0, 255), stop:0.339795 rgba(255, 0, 0, 255), stop:0.339799 rgba(255, 255, 255, 255), stop:0.662444 rgba(255, 255, 255, 255), stop:0.662469 rgba(0, 0, 255, 255), stop:1 rgba(0, 0, 255, 255));")
+        self.label_2.setObjectName("label_2")
+        self.bili_bun = QtWidgets.QPushButton(self.frame)
+        self.bili_bun.setGeometry(QtCore.QRect(390, 120, 93, 28))
+        self.bili_bun.setObjectName("bili_bun")
+        self.baidu_btn = QtWidgets.QPushButton(self.frame)
+        self.baidu_btn.setGeometry(QtCore.QRect(390, 170, 93, 28))
+        self.baidu_btn.setObjectName("baidu_btn")
+        self.douyin_btn = QtWidgets.QPushButton(self.frame)
+        self.douyin_btn.setGeometry(QtCore.QRect(390, 220, 93, 28))
+        self.douyin_btn.setObjectName("douyin_btn")
+        self.tengxun_btn = QtWidgets.QPushButton(self.frame)
+        self.tengxun_btn.setGeometry(QtCore.QRect(390, 270, 93, 28))
+        self.tengxun_btn.setObjectName("tengxun_btn")
+        home.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(home)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        home.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(home)
+        self.statusbar.setObjectName("statusbar")
+        home.setStatusBar(self.statusbar)
+
+        self.retranslateUi(home)
+        self.pushButton_2.clicked.connect(home.showMinimized)
+        QtCore.QMetaObject.connectSlotsByName(home)
+
+    def retranslateUi(self, home):
+        _translate = QtCore.QCoreApplication.translate
+        home.setWindowTitle(_translate("home", "MainWindow"))
+        self.login_btn.setText(_translate("home", "✈"))
+        self.pushButton_2.setText(_translate("home", "-"))
+        self.exit.setText(_translate("home", "X"))
+        self.label.setText(_translate("home", "Logo"))
+        self.label_2.setText(_translate("home", "欢迎来到我的主页"))
+        self.bili_bun.setText(_translate("home", "哔哩哔哩"))
+        self.baidu_btn.setText(_translate("home", "百度"))
+        self.douyin_btn.setText(_translate("home", "抖音"))
+        self.tengxun_btn.setText(_translate("home", "腾讯"))
+
--- demo/main.py
+++ demo/main.py
@@ -1,11 +1,28 @@
 from login_ui import Ui_login  # 替换为你的 UI 类名
+from  home_ui import Ui_home # 替换为你的主窗口类名
+import pymysql
 import sys
+import webbrowser
 from PyQt5.QtWidgets import QMainWindow, QApplication   
 import PyQt5.QtCore as QtCore
+conn = pymysql.Connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='',
+    db='ceshi_login',
+    charset='utf8'
+)
+cur = conn.cursor()
+cur.execute("select * from user")
+result = cur.fetchall()
+print(result)
+
 class Loginwindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_login()  # 使用正确的 UI 类
+        # self.ui = Ui_home()  # 使用正确的 UI 类
 
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 无边框窗口
@@ -13,10 +30,74 @@ class Loginwindow(QMainWindow):
         # self.ui.pushButton_2.clicked.connect(self.close)  # 关闭窗口
         self.ui.pushButton_5.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))      # 显示主窗口
         self.ui.pushButton_6.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))      # 显示注册窗口
-
+        self.ui.pushButton.clicked.connect(self.logintohome)  # 登录到主页
+        self.ui.pushButton_2.clicked.connect(self.zhuce)  # 登录到主页
         self.show()
+    def zhuce(self):
+                username = self.ui.lineEdit_4.text()
+                password = self.ui.lineEdit_9.text()
+                password1 = self.ui.lineEdit_3.text()
+                # 直接获取输入框内容，避免多次调用方法
+                if username and password==password1:
+                    try:
+                        # 使用参数化查询插入用户
+                        cur.execute("INSERT INTO user(user, password) VALUES(%s, %s)", (username, password))
+                        conn.commit()  # 提交事务
+                        print('注册成功')
+                    except pymysql.Error as e:
+                        print(f'注册过程中发生错误: {e}')
+                else:
+                    print('用户名和密码不能为空')
+    def logintohome(self):
+        lineEdit = self.ui.lineEdit.text()
+        lineEdit_2 = self.ui.lineEdit_2.text()
 
+        try:
+            # 直接判断结果集是否含有对应的用户名和密码
+            user_found = next((i for i in result if lineEdit == i[0] and lineEdit_2 == i[1]), None)
+
+            if user_found:
+                print('登录成功')
+                self.ui = homewindow()
+                self.close()
+            else:
+                print('用户名或密码错误')
+        except Exception as e:
+            print(f'登录过程中发生错误: {e}')
+            
+
+        
+        
+        # self.ui.stackedWidget.setCurrentIndex(0)
+        
+class homewindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui =  Ui_home()  # 使用正确的 UI 类
+        self.ui.setupUi(self)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 无边框窗口
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 透明背景
+        # self.ui.pushButton.clicked.connect(self.logintohome)  # 关闭窗口
+        # self.ui.pushButton_5.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))      # 显示主窗口
+        # self.ui.pushButton_6.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))      # 显示注册窗口
+        self.ui.bili_bun.clicked.connect(lambda: webbrowser.open('https://www.bilibili.com'))
+        self.ui.baidu_btn.clicked.connect(lambda: webbrowser.open('https://www.baidu.com'))
+        self.ui.douyin_btn.clicked.connect(lambda: webbrowser.open('https://www.douyin.com'))
+        self.ui.tengxun_btn.clicked.connect(lambda: webbrowser.open('https://www.qq.com'))
+        self.ui.login_btn.clicked.connect(self.tologin) 
+        self.ui.exit.clicked.connect(self.close)
+        self.show()
+    def close(self):
+        # self.ui = Loginwindow()
+        self.close()
+        exit()
+
+    def tologin(self):
+        self.ui = Loginwindow()
+        self.close()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = Loginwindow()  # 实例化登录窗口
     sys.exit(app.exec_())
+    for i in range(10):
+        print('test')
