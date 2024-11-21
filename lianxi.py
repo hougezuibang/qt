de0ddf2bf6ae4c6dadce5decc94743ff4aa17c78
hougezuibang <3427781715@qq.com>
2024-11-21 13:47:26 +0800

    1

---
+++ lianxi.py
@@ -0,0 +1,39 @@
+# print("qwew","errerw","qwed","asfgvdfhbfg",sep="123")
+# age=10
+# name='拉克丝'
+# print("我的名字%s，年龄%d"%(name,age))
+#
+#
+# li=[1,2,3,4]
+# li2=li
+# li2.append(5)
+# print(li)
+#
+# print(li2)
+# def fibonacci(df):
+#     """生成斐波那契数列的前n个数"""
+#
+#     def fib_sequence1(name1):
+#         print("1111111n"+name1)
+#         df()
+#     return fib_sequence1
+# print(33333)
+#
+# @fibonacci
+# def fib_sequence():
+#     print(111111)
+# fib_sequence("qq333qqq")
+
+
+
+class Test:
+    def __init__(self):
+        print("init")
+    def test(self,name):
+        print("hello world"+name)
+    # def __del__(self):
+    #     print("del")
+tt=Test()
+tt.test("王五")
+# del tt
+tt.test("王五22")
