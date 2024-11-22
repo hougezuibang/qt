3c87196b77e60781315237561b7ac450cb00abd9
hougezuibang <3427781715@qq.com>
2024-11-22 10:02:14 +0800

    1

--- UI/邮票孔.py
+++ UI/邮票孔.py
@@ -18,9 +18,6 @@ class Ui_Form(QtWidgets.QWidget):
         Form.setObjectName("Form")
         Form.resize(741, 504)
         Form.move(1200, 500)
-        # Form.move(1200, 500)
-        # Form.move(1200, 500)
-        # Form.move(1200, 500)
         JobName = '2s'
         step = ['set']
         # 设置自定义特征
